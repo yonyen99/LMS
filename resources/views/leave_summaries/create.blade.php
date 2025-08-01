32e8e96ad341cbcfc068ecc3e76a87cce9d32e58
Chhea012 <chhea.chhouy@student.passerellesnumeriques.org>
2025-08-01 15:30:04 +0700

    feature: update new style interface about summary and leave type [SCRUM-50]

--- resources/views/leave_summaries/create.blade.php
+++ resources/views/leave_summaries/create.blade.php
@@ -1,69 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card card-2 "  style="box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;">
-        <div class="card-header card-2 fw-bold">Create Leave Summary</div>
-
-        <div class="card-body">
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul class="mb-0">
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
-            <form action="{{ route('leave-summaries.store') }}" method="POST">
-                @csrf
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <label for="department_id" class="form-label">Department</label>
-                        <select name="department_id" id="department_id" class="form-select" required>
-                            <option value="">Select Department</option>
-                            @foreach ($departments as $department)
-                                <option value="{{ $department->id }}"
-                                    {{ old('department_id') == $department->id ? 'selected' : '' }}>
-                                    {{ $department->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="entitled" class="form-label">Entitled Days</label>
-                        <input type="number" step="0.5" name="entitled" id="entitled" class="form-control"
-                            value="{{ old('entitled') }}" required>
-                    </div>
-                </div>
-
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <label for="leave_type_id" class="form-label">Leave Type</label>
-                        <select name="leave_type_id" id="leave_type_id" class="form-select" required>
-                            <option value="">Select Leave Type</option>
-                            @foreach ($leaveTypes as $type)
-                                <option value="{{ $type->id }}"
-                                    {{ old('leave_type_id') == $type->id ? 'selected' : '' }}>
-                                    {{ $type->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="report_date" class="form-label">Report Date</label>
-                        <input type="date" name="report_date" id="report_date" class="form-control"
-                            value="{{ old('report_date', now()->format('Y-m-d')) }}" required>
-                    </div>
-                </div>
-
-                <div class="d-flex gap-3">
-                    <button type="submit" class="btn btn-primary">Create</button>
-                    <a href="{{ route('leave-summaries.index') }}" class="btn btn-warning text-white">Cancel</a>
-                </div>
-            </form>
-        </div>
+<div class="container-fluid">
+    <div class="alert alert-info text-center">
+        <p>The leave summary creation form is now accessible via the "Add Leave Summary" button on the <a href="{{ route('leave-summaries.index') }}">Leave Summaries</a> page.</p>
     </div>
 </div>
 @endsection
--- resources/views/leave_types/edit.blade.php
+++ resources/views/leave_types/edit.blade.php
@@ -1,9 +1,10 @@
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container-fluid">
     <div class="alert alert-info text-center">
-        <p>The leave type edit form is now accessible via the "Edit" action on the <a href="{{ route('leave-types.index') }}">Leave Types</a> page.</p>
+        <p>The leave type details are now accessible via the "View" action on the <a href="{{ route('leave-types.index') }}">Leave Types</a> page.</p>
     </div>
 </div>
 @endsection
